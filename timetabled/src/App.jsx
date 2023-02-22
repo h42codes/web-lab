6c286b70f8d5e6fd2f6becdeb0dc5d37b78e71a5
h42codes <99951343+h42codes@users.noreply.github.com>
2023-02-22 11:36:35 +0900

    Add Calendar component

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Calendar from "./components/Calendar";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
         Welcome to Chicago, Natalia! Check out this calendar to get to know the
         city and see all the sights during your stay.
       </h2>
+      <Calendar />
     </div>
   );
 };
