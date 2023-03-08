969c31c54277728802fbe0f52ade716bfddb8c40
h42codes <99951343+h42codes@users.noreply.github.com>
2023-03-08 18:34:27 +0900

    Create BaristaForm component

--- barista-app/src/App.jsx
+++ barista-app/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import BaristaForm from "./components/BaristaForm";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="title-container">
+        <div className="title">Oh My Grind</div>
+        <p>So you think you can barista? Let's put that to the test...</p>
+      </div>
+      <BaristaForm />
+    </div>
+  );
 }
 
 export default App;
