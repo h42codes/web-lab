af2f7ac3932db4cea272ba6fb150b57bf0968015
h42codes <99951343+h42codes@users.noreply.github.com>
2023-03-08 18:36:56 +0900

    Add state to keep track of user choices

--- barista-app/src/components/BaristaForm.jsx
+++ barista-app/src/components/BaristaForm.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 const BaristaForm = () => {
+  const [inputs, setInputs] = useState({
+    temperature: "",
+    milk: "",
+    syrup: "",
+    blended: "",
+  });
+
+  const ingredients = {
+    temperature: ["hot", "lukewarm", "cold"],
+    syrup: ["mocha", "vanilla", "toffee", "maple", "caramel", "other", "none"],
+    milk: ["cow", "oat", "goat", "almond", "none"],
+    blended: ["yes", "turbo", "no"],
+  };
+
   const onCheckAnswer = () => {};
 
   const onNewDrink = () => {};
