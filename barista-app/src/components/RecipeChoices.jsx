c0c8def3097a77c1d88ed9fbc4f704e7f5b6db32
h42codes <99951343+h42codes@users.noreply.github.com>
2023-03-13 16:27:44 +0900

    Switch from radio buttons to user inputs

--- barista-app/src/components/RecipeChoices.jsx
+++ barista-app/src/components/RecipeChoices.jsx
@@ -4,20 +4,15 @@ const RecipeChoices = ({ handleChange, label, choices, checked }) => {
   return (
     <div>
       <div className="radio-buttons">
-        {choices &&
-          choices.map((choice) => (
-            <li key={choice}>
-              <input
-                id={choice}
-                value={choice}
-                name={label}
-                type="radio"
-                onChange={handleChange}
-                checked={checked == choice}
-              />
-              {choice}
-            </li>
-          ))}
+        <input
+          type="text"
+          name={label}
+          value={checked}
+          placeholder="Guess the ingredient..."
+          onChange={handleChange}
+          className="textbox"
+        />
+        {choices && choices.map((choice) => <li key={choice}>{choice}</li>)}
       </div>
     </div>
   );
