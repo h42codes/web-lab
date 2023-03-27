ce843d7303229f406f3c456bbc918f21473a1e10
h42codes <hyungjikim42@gmail.com>
2023-03-27 14:35:18 -0400

    Initialize Crypto Hustle

---
+++ crypto-hustle/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="App"></div>;
+}
+
+export default App;
