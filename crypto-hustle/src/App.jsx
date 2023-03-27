9e0b8f3ddbf7252f7457bc20c6e478322c52a2f0
h42codes <hyungjikim42@gmail.com>
2023-03-27 14:50:58 -0400

    Display crypto list

--- crypto-hustle/src/App.jsx
+++ crypto-hustle/src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [list, setList] = useState(null);
 
-  return <div className="App"></div>;
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch(
+        "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" + API_KEY
+      );
+      const json = await response.json();
+      setList(json);
+    };
+
+    fetchAllCoinData().catch(console.error);
+  }, []);
+
+  return (
+    <div className="whole-page">
+      <h1>My Crypto List</h1>
+      <ul>
+        {list &&
+          Object.entries(list.Data).map(([coin]) =>
+            list.Data[coin].PlatformType === "blockchain" ? (
+              <li key={list.Data[coin].FullName}>{list.Data[coin].FullName}</li>
+            ) : null
+          )}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
