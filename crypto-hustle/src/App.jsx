a29950b3a35d301eb7440b6f2bdf441251acc306
h42codes <hyungjikim42@gmail.com>
2023-03-27 14:59:58 -0400

    Add CoinInfo component

--- crypto-hustle/src/App.jsx
+++ crypto-hustle/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import CoinInfo from "./components/CoinInfo";
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
@@ -24,7 +25,12 @@ function App() {
         {list &&
           Object.entries(list.Data).map(([coin]) =>
             list.Data[coin].PlatformType === "blockchain" ? (
-              <li key={list.Data[coin].FullName}>{list.Data[coin].FullName}</li>
+              // <li key={list.Data[coin].FullName}>{list.Data[coin].FullName}</li>
+              <CoinInfo
+                image={list.Data[coin].ImageUrl}
+                name={list.Data[coin].FullName}
+                symbol={list.Data[coin].Symbol}
+              />
             ) : null
           )}
       </ul>
