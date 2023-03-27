0e1c9b794b1a0b9a12ae14df5aa8d115cc619134
h42codes <hyungjikim42@gmail.com>
2023-03-27 11:35:10 -0400

    Add APIForm component

--- cap/src/App.jsx
+++ cap/src/App.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
 import "./App.css";
+import APIForm from "./components/APIForm";
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
 function App() {
-  return <div className="App"></div>;
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
+
+  const submitForm = () => {};
+
+  return (
+    <div className="whole-page">
+      <h1>Build Your OWn Screenshot!</h1>
+      <APIForm
+        inputs={inputs}
+        handleChange={(e) =>
+          setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        onSubmit={submitForm}
+      />
+      <br />
+    </div>
+  );
 }
 
 export default App;
