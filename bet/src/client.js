6fb125c225aa12b26437f8edfa7ecbf74e2d20dd
h42codes <hyungjikim42@gmail.com>
2023-04-12 17:49:32 -0400

    Implement CreatePost

--- bet/src/client.js
+++ bet/src/client.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const URL = "https://zbmoaenbaebcnwlkhmys.supabase.co";
 
-API_KEY =
+const API_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpibW9hZW5iYWViY253bGtobXlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY3MDQ3NjgsImV4cCI6MTk5MjI4MDc2OH0.ywiG1mmzjA8g8r04mpRiJtlFzgH0HDKB4W6lDQxR26I";
 
 export const supabase = createClient(URL, API_KEY);
--- bet/src/pages/CreatePost.js
+++ bet/src/pages/CreatePost.js
@@ -1,27 +1,65 @@
-import React from 'react';
-import './CreatePost.css'
+import React, { useState } from "react";
+import "./CreatePost.css";
+import { supabase } from "../client";
 
 const CreatePost = () => {
+  const [post, setPost] = useState({
+    title: "",
+    author: "",
+    description: "",
+  });
 
-    return (
-        <div>
-            <form>
-                <label for="title">Title</label> <br />
-                <input type="text" id="title" name="title" /><br />
-                <br/>
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setPost((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  };
 
-                <label for="author">Author</label><br />
-                <input type="text" id="author" name="author" /><br />
-                <br/>
+  const createPost = async (event) => {
+    event.preventDefault();
 
-                <label for="description">Description</label><br />
-                <textarea rows="5" cols="50" id="description">
-                </textarea>
-                <br/>
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-    )
-}
+    await supabase
+      .from("Posts")
+      .insert({
+        title: post.title,
+        author: post.author,
+        description: post.description,
+      })
+      .select();
 
-export default CreatePost
+    window.location = "/";
+  };
+
+  return (
+    <div>
+      <form>
+        <label for="title">Title</label> <br />
+        <input type="text" id="title" name="title" onChange={handleChange} />
+        <br />
+        <br />
+        <label for="author">Author</label>
+        <br />
+        <input type="text" id="author" name="author" onChange={handleChange} />
+        <br />
+        <br />
+        <label for="description">Description</label>
+        <br />
+        <textarea
+          rows="5"
+          cols="50"
+          id="description"
+          name="description"
+          onChange={handleChange}
+        ></textarea>
+        <br />
+        <input type="submit" value="Submit" onClick={createPost} />
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
